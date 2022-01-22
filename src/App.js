2dc90a9d1cd1049532c2da6e1feeb3903ee1de51
Andi Bašić <andi.basic10@gmail.com>
2022-01-22 20:07:28 +0100

    FeedbackProvider

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import About from "./pages/About"
 import AboutIconLink from "./components/AboutIconLink"
+import {FeedbackProvider} from './context/FeedbackContext'
+
+
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
@@ -27,26 +30,28 @@ function App() {
   }
 
   return (
-    <Router>
-      <Header text="Hello world" />
-      <div className="container">
-      <Routes>
-        <Route 
-          exact 
-          path='/' 
-          element={
-          <>
-            <FeedbackForm handleAdd={addFeedback} />
-            <FeedbackStats feedback={feedback}/>
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-          </>
-        }>
-        </Route>
-        <Route path='/about' element={<About />} />
-      </Routes>
-      <AboutIconLink/>
-      </div>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Header text="Hello world" />
+        <div className="container">
+        <Routes>
+          <Route 
+            exact 
+            path='/' 
+            element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback}/>
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          }>
+          </Route>
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <AboutIconLink/>
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
