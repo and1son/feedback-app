b051dbda5e47ab6143c524bab8504b0888050ac9
and1son <andi.basic10@gmail.com>
2022-01-25 13:47:56 +0100

    fetch POST api

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -19,7 +18,7 @@ export const FeedbackProvider = ({ children }) => {
     }, [])
 
     const fetchFeedback = async () => {
-        const response = await fetch('http://localhost:5001/feedback?_sort=id?&_order=desc')
+        const response = await fetch('/feedback?_sort=id?&_order=desc')
         const data = await response.json()
         setFeedback(data)
         setIsLoading(false)
@@ -33,10 +32,18 @@ export const FeedbackProvider = ({ children }) => {
         }
     };
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        console.log(newFeedback)
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback),
+        })
+        //console.log("nF", newFeedback)
+        const data = await response.json()
+        //console.log("data", JSON.stringify(newFeedback))
+        setFeedback([data, ...feedback])
     }
 
     const editFeedback = (item) => {
